68e1427ea81177f93bb4a03d1bc123e94731de7d
nishio.n0522 <nishio.n0522@gmail.com>
2023-09-19 08:48:34 +0900

    update_db_design

--- db/race_results.py
+++ db/race_results.py
@@ -1,11 +1,43 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, Text
+from sqlalchemy import Column, Integer, String, Float, Date, Text, Numeric
 
 from db.setting import Engine
 from db.setting import Base
 
-class User(Base):
+class BoatRaceData(Base):
     """
     レース結果
+
+    stadium_id: Integer
+        ボートレース場id
+    date: Date
+        レース開催日
+    race_inedex: Integer
+        レース番号
+    series_name: Text
+        節名
+    weather_id: Integer
+        天気id
+    wind_direction_id: Integer
+        風向id
+    wind_speed: Float
+        風速 [m/s]
+    wave_height: Float
+        波高
+    win_refund: Integer
+        単勝払い戻し
+    place_refund: Integer
+        複勝払い戻し
+    perfecta_refund: Integer
+        2連単払い戻し
+    quinella_refund: Integer
+        2連複払い戻し
+    boxed_quinella_refund: Integer
+        拡連複払い戻し
+    trifecta_refund: Integer
+        3連単払い戻し
+    boxed_trifecta_refund: Integer
+        3連複払い戻し
+    
     """
 
     __tablename__ = 'race_results'
@@ -14,10 +46,20 @@ class User(Base):
     }
 
     stadium_id = Column('stadium_id', Integer, primary_key=True)
-    date = Column('date', DateTime, primary_key=True)
+    date = Column('date', Date, primary_key=True)
     race_index = Column('race_index', Integer, primary_key=True)
     series_name = Column('series_name', Text)
-    weather = Column('weather', )
+    weather = Column('weather', Text)
+    wind_direction = Column('wind_direction', Text)
+    wind_speed = Column('wind_speed', Float)
+    wave_height = Column('wave_height', Float)
+    win_refund = Column('win_refund', Integer)
+    place_refund = Column('place_refund', Integer)
+    perfecta_refund = Column('perfecta_refund', Integer)
+    quinella_refund = Column('quinella_refund', Integer)
+    boxed_quinella_refund = Column('boxed_quinella_refund', Integer)
+    trifecta_refund = Column('trifecta_refund', Integer)
+    boxed_trifecta_refund = Column('boxed_trifecta_refund', Integer)
 
 
 if __name__ == "__main__":
